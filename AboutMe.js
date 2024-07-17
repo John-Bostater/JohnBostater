41dcbf4451f61853ab1986e3db4810b2352f744f
John Bostater <jdbost8r@gmail.com>
2024-07-16 19:50:50 -0700

    Add files via upload

---
+++ AboutMe.js
@@ -0,0 +1,34 @@
+/*
+[Author]: John Bostater
+
+[Start Date]: 7/16/24
+
+[Description]:
+    This file will contain all of the animation and event-handling syntax
+    for the html document.
+
+[Notes]:
+    //Any programming notes should go here...
+
+    //Add a button to navigate back to the welcome page html 
+*/
+
+
+//Action-Event Handling
+//-------------------------------------------------------------
+
+//Go Back Button
+  function goBack(){
+
+  }
+//
+
+
+//-------------------------------------------------------------
+
+
+//Animations
+//-------------------------------------------------------------
+
+
+//-------------------------------------------------------------
